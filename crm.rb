7ce9dcb619f72810c50b211759c7079031a6ea01
Kathleen <kathleenmcclay@gmail.com>
2017-07-12 15:54:28 -0400

    completed contacts added about pae COMPLETE' '

--- crm.rb
+++ crm.rb
@@ -4,3 +4,21 @@ require 'sinatra'
 get '/index' do
   erb :index
 end
+
+get '/contacts' do
+
+  erb :contacts
+
+end
+
+get '/about' do
+
+  erb :about
+end
+
+
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
